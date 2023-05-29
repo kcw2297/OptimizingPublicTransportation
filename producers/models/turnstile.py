bd723c2daee3be77a368b86d193e9198f15279bb
kcw2297 <chaeugg@gmail.com>
2023-05-29 21:01:12 +0900

    Add : Turnstile Class Created

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -1,4 +1,3 @@
-"""Creates a turnstile data producer"""
 import logging
 from pathlib import Path
 
@@ -13,17 +12,15 @@ logger = logging.getLogger(__name__)
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
-        """Create the Turnstile"""
-        station_name = (
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+        STATIONTOPIC = (
             station.name.lower()
             .replace("/", "_and_")
             .replace(" ", "_")
@@ -31,29 +28,22 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            STATIONTOPIC,
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=1,
+            num_replicas=1,
         )
-        self.station = station
-        self.turnstile_hardware = TurnstileHardware(station)
 
     def run(self, timestamp, time_step):
-        """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "timestamp": timestamp,
+                "time_step": time_step,
+                "num_entries": num_entries,
+            },
+        )
