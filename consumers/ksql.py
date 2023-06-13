c8aeb83f5df9892d1c829851af8a4830d6959d2c
kcw2297 <chaeugg@gmail.com>
2023-06-14 00:27:28 +0900

    Add : consumer 초안 작성

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -8,14 +8,6 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
@@ -36,11 +28,9 @@ WITH (VALUE_FORMAT='JSON') AS
 """
 
 
-
-
 def execute_statement():
     """Executes the KSQL statement against the KSQL API"""
-    if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
+    if topic_check.topic_exists("TURNSTILE_SUMMARY"):
         return
 
     logging.debug("executing ksql statement...")
